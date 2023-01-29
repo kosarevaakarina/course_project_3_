3d3db71409a001657757a43ef16763d98f82d56a
user <kosareva.karina2013@yandex.ru>
2023-01-29 20:47:13 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from func import *
+
+data = open_file('operations.json')
+sorted_data = sorted_by_executed_and_date(data)
+
+for i in sorted_data:
+    print(f"\n{formatted_date(i)} {get_description(i)}")
+    print(f"{get_sender(i)} -> {get_recipient(i)}")
+    print(f"{get_transfer_amount(i)} {get_currency(i)}")
 
